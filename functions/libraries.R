de900a2b9a096ae0bfb89dfea14e9fdcb88791ef
kat rivas <kationne@gmail.com>
2020-09-11 09:57:40 -0700

    week 3 homework

---
+++ functions/libraries.R
@@ -0,0 +1,14 @@
+# libraries ...
+
+library(pryr)     #mem_used()
+library(lmtest)   #lmtest has lrtest
+lrtest
+library(MASS)     #polr
+
+library(tractor.base)   #implode
+
+library(stringr)
+library(rvest)    #imdb web scraping
+
+library(dplyr)    #data.frame manipulation for personality
+library(readr)    #read personality txt file
