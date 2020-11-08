cb3d1488f32d65bd0d7035a24e91dc0ff4f0c265
kat rivas <kationne@gmail.com>
2020-11-08 10:22:29 -0800

    argh

--- functions/functions-project-measure.R
+++ functions/functions-project-measure.R
@@ -1,55 +1,55 @@
 
 prepareMeasureData = function(x)
 {
-  subset.df = select(x, head.height, height, my.ethnicity, age, my.gender)
-  subset.df$head.to.height = (subset.df$height - subset.df$head.height) / subset.df$head.height
-  subset.df$ethnicity.groups <- NA
+  myData = select(measure, head.height, height, my.ethnicity, age, my.gender)
+  myData$head.to.height = (myData$height - myData$head.height) / myData$head.height
+  myData$ethnicity.groups <- NA
   
-  for (i in 1:length(subset.df$my.ethnicity))
+  for (i in 1:length(myData$my.ethnicity))
   {
-    if (subset.df[i, 3] == "w")
+    if (myData[i, 3] == "w")
     {
-      subset.df[i, 7] = "1"  #1 for white
+      myData[i, 7] = "1"  #1 for white
     }
-    if (subset.df[i, 3] == "a" | subset.df[i, 3] == "j" | subset.df[i, 3] == "lao" |
-        subset.df[i, 3] == "k" | subset.df[i, 3] == "c" | subset.df[i, 3] == "f" )
+    if (myData[i, 3] == "a" | myData[i, 3] == "j" | myData[i, 3] == "lao" |
+        myData[i, 3] == "k" | myData[i, 3] == "c" | myData[i, 3] == "f" )
     {
-      subset.df[i, 7] = "a"  #2 for asian,but not indian
+      myData[i, 7] = "2"  #2 for asian,but not indian
     }
-    if (subset.df[i, 3] == "al" | subset.df[i, 3] == "ca" | subset.df[i, 3] == "i" | subset.df[i, 3] == "ji" | 
-        subset.df[i, 3] == "nat" | subset.df[i, 3] == "pi")
+    if (myData[i, 3] == "al" | myData[i, 3] == "ca" | myData[i, 3] == "i" | myData[i, 3] == "ji" | 
+        myData[i, 3] == "nat" | myData[i, 3] == "pi")
     {
-      subset.df[i, 7] = "5"  #5 for other, i.e. mixed, indian, native american
+      myData[i, 7] = "5"  #5 for other, i.e. mixed, indian, native american
     }
-    if (subset.df[i, 3] == "b")
+    if (myData[i, 3] == "b")
     {
-      subset.df[i, 7] = "4"  #4 for african american
+      myData[i, 7] = "4"  #4 for african american
     }
-    if (subset.df[i, 3] == "h" | subset.df[i, 3] == "l" )
+    if (myData[i, 3] == "h" | myData[i, 3] == "l" )
     {
-      subset.df[i, 7] = "3"  #3 for hispanic
+      myData[i, 7] = "3"  #3 for hispanic
     }
-  }
-  
-  for (j in 1:length(subset.df$my.gender))
-  {
-    if (subset.df[i,5] == "f")
+    #measure.df = prepareMeasureData(measure)
+    for (j in 1:length(myData$my.gender))
     {
-      subset.df[i,5] == "1"
-    }
-    if (subset.df[i,5] == "m")
-    {
-      subset.df[i,5] == "2"
-    }
-    if (subset.df[i,5] == "o")
-    {
-      subset.df[i,5] == "3"
+      if (myData[i,5] == "f")
+      {
+        myData[i,5] == "1"
+      }
+      if (myData[i,5] == "m")
+      {
+        myData[i,5] == "2"
+      }
+      if (myData[i,5] == "o")
+      {
+        myData[i,5] == "3"
+      }
     }
   }
   
   #remove NAs
-  subset.df = na.omit(subset)
+  myData = na.omit(myData)
   
   #remove my.ethnicity because it's not numeric for correlation table
-  subset.df = subset[subset.df ,select = -c(my.ethnicity)]
+  myData = subset(myData, select = -my.ethnicity)
 }
